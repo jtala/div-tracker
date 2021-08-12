1be0dbf106841f36c1d11e657424db35ce393347
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-08-12 15:18:08 -0700

    Setting up axios call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Axios from "axios";
 import Begin from "./components/Begin"
+import Dividends from "./components/Dividends"
 import './App.css';
 
 function App() {
+
+  const getStockData = () => {
+    Axios.get()
+  }
+
   return (
     <div>
       <Begin></Begin>
+      <Dividends></Dividends>
     </div>
   );
 }
---
+++ src/components/Dividends.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Dividends = () => (
+    <div>VTI Holdings
+    
+    </div>
+)
+
+export default Dividends;
