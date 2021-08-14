adc114bff18df46e37b03d57c6351e9d50298b8a
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-08-13 23:35:14 -0700

    Doing nothing special:

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Axios from "axios";
 import Begin from "./components/Begin"
 import Dividends from "./components/Dividends"
@@ -6,20 +6,24 @@ import './App.css';
 
 function App() {
 
+  const [data, getData] = useState("");
+
   const getStockData = () => {
     Axios.get("https://eodhistoricaldata.com/api/fundamentals/VTI.US?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX").then((response) => {
-      console.log(response.data.ETF_Data.Holdings);
-
-
-      // 1. Want to get name, ticker
-    })
-  }
+      let holdings = response.data.ETF_Data.Holdings["Apple Inc"].Code;
+      console.log(holdings);
+      return getData[holdings];
+    }
+    )
+  };
 
   return (
     <div>
       <Begin></Begin>
       <button onClick={getStockData}>Click for VTI Data</button>
+      
       <Dividends></Dividends>
+      {data}
     </div>
   );
 }
