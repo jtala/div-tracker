c8dedc66cd1eb6cd3b561d183b649992e0f9e097
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-08-16 18:02:28 -0700

    Div progress

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React, {useState} from "react";
 import Axios from "axios";
 import Begin from "./components/Begin"
-import Dividends from "./components/Dividends"
 import './App.css';
 
 function App() {
 
-  const [data, getData] = useState("");
+  const [data, updateData] = useState("Default State here");
+  let holdings; 
 
   const getStockData = () => {
     Axios.get("https://eodhistoricaldata.com/api/fundamentals/VTI.US?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX").then((response) => {
-      let holdings = response.data.ETF_Data.Holdings;
+
+      holdings = response.data.ETF_Data.Holdings;
       
-      for (let [key,value] of Object.entries(holdings)){
-        console.log(key, value);
-      }
-    }
-    )
+  }) 
   };
+
+  const updateHoldings = () => {
+    // for (let [key,value] of Object.entries(holdings)){
+    //   console.log(key, value);
+    // }
+
+    
+    // updateData(holdings.toString());
+  }
+
 
   return (
     <div>
       <Begin></Begin>
       <button onClick={getStockData}>Click for VTI Data</button>
-      
-      <Dividends></Dividends>
-      {data}
+      <button onClick={updateHoldings}>Click to refresh data</button>
+      <h1>{data}</h1> 
     </div>
   );
 }
