41481d14e975a6413607ec9af9a4c39c6b6cb6ff
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-08-16 21:05:24 -0700

    Trying to get data to map over properly

--- src/App.js
+++ src/App.js
@@ -2,34 +2,44 @@ import React, {useState} from "react";
 import Axios from "axios";
 import Begin from "./components/Begin";
 // import Dividends from "./components/Dividends";
+import {Card} from "react-bootstrap";
 
 
 import './App.css';
 
 function App() {
 
-  const [data, updateData] = useState("Default State here");
-  let holdings; 
+  const [data, updateData] = useState([]);
+  let holdings;
+  let testObj = [
+    {Name: 1},
+    {Code: 2},
+    {3: 3},
+    {4: 4}
+  ];
 
   const getStockData = () => {
-    Axios.get("https://eodhistoricaldata.com/api/fundamentals/VTI.US?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX").then((response) => {
+    Axios.get("https://eodhistoricaldata.com/api/fundamentals/VTI.US?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX")
+    .then((response) => {
 
-      holdings = response.data.ETF_Data.Holdings;
-      
+
+    holdings = Object.values(response.data.ETF_Data.Holdings);
+    console.log(holdings[0]);
   }) 
   };
 
-  const updateHoldings = () => {
-    let secName;
-    let secData; 
+  const renderCards = (card, index) => {
 
-    for (let [key,value] of Object.entries(holdings)){
-      console.log(value.Code, value.Name);
-    }
 
-    
-
-    // updateData(JSON.stringify(holdings));
+    return (
+      <Card style={{ width: '18rem' }} key={index}>
+        <Card.Img variant="top"/>
+        <Card.Body>
+          <Card.Title>{card.Name}</Card.Title>
+          <Card.Text>{card.Code}</Card.Text>
+        </Card.Body>
+      </Card>
+    )
   }
 
 
@@ -37,8 +47,7 @@ function App() {
     <div>
       <Begin></Begin>
       <button onClick={getStockData}>Click for VTI Data</button>
-      <button onClick={updateHoldings}>Click to refresh data</button>
-      {data}
+      {testObj.map(renderCards)}
     </div>
   );
 }
