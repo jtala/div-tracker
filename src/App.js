407bd71a9ce5b593f83b0f1410926f3ae58ab7df
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-08-17 17:36:15 -0700

    getStockData work, need to research state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {useState} from "react";
 import Axios from "axios";
 import Begin from "./components/Begin";
-// import Dividends from "./components/Dividends";
-import {Card} from "react-bootstrap";
 
 
 import './App.css';
@@ -11,43 +9,23 @@ function App() {
 
   const [data, updateData] = useState([]);
   let holdings;
-  let testObj = [
-    {Name: 1},
-    {Code: 2},
-    {3: 3},
-    {4: 4}
-  ];
+ 
 
-  const getStockData = () => {
+  const getStockData = (holdings = []) => {
     Axios.get("https://eodhistoricaldata.com/api/fundamentals/VTI.US?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX")
     .then((response) => {
 
 
     holdings = Object.values(response.data.ETF_Data.Holdings);
-    console.log(holdings[0]);
+    console.log(holdings);
   }) 
   };
-
-  const renderCards = (card, index) => {
-
-
-    return (
-      <Card style={{ width: '18rem' }} key={index}>
-        <Card.Img variant="top"/>
-        <Card.Body>
-          <Card.Title>{card.Name}</Card.Title>
-          <Card.Text>{card.Code}</Card.Text>
-        </Card.Body>
-      </Card>
-    )
-  }
 
 
   return (
     <div>
       <Begin></Begin>
       <button onClick={getStockData}>Click for VTI Data</button>
-      {testObj.map(renderCards)}
     </div>
   );
 }
